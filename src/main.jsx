9d92f9769f06e5c03e9ee582e80823dc4b0e86cc
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2024-02-18 12:49:46 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import { Router } from "./Routers/Router";
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={Router} />
+  </React.StrictMode>
+);
